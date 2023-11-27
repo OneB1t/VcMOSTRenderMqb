1f93e5b4e54e9e8d13d8963111ab0b114be504b2
Jan Žďárský <jzdarsky@simpleway.cz>
2023-11-28 00:43:45 +0100

    AA icons for all maneuvrs

--- VCRenderAADash.py
+++ VCRenderAADash.py
@@ -12,22 +12,8 @@ width = 800
 height = 480
 
 # Data Position
-driveLevelPos = "i:1304:211"
-nightModePos = "i:1304:212"
 distancePos = "i:1304:213"
-gearPos = "s:1304:214"
-fuelLevelLowPos = "i:1304:215"
 speedPos = "i:1304:216"
-satellitesInViewPos = "i:1304:217"
-satellitesInUsePos = "i:1304:218"
-gpsLatPos = "i:1304:219"
-gpsLongPos = "i:1304:220"
-gpsAccPos = "i:1304:221"
-gpsAltPos = "i:1304:222"
-gpsSpeedPos = "i:1304:223"
-gpsBearingPos = "i:1304:224"
-parkingBrakePos = "i:1304:225"
-gpsTimestampPos = "s:1304:226"
 
 
 # Function to set a pixel to white (255) at the specified coordinates
@@ -85,8 +71,54 @@ font = {
     ord('_'): [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x3E]
 }
 
+icons = {
+    ord('0'): [64, 0, 0, 0, 1, 0, 0, 0, 1, 64, 0, 0, 0, 1, 64, 0, 0, 5, 65, 0, 0, 0, 1, 64, 0, 0, 21, 65, 0, 0, 0, 1, 68, 0, 0, 42, 65, 0, 0, 0, 1, 4, 0, 0, 80, 65, 0, 0, 0, 8, 132, 0, 0, 0, 32, 0, 0, 0, 16, 128, 0, 0, 0, 160, 0, 0, 0, 32, 128, 0, 0, 4, 162, 0, 0, 0, 0, 136, 0, 0, 10, 130, 0, 0, 1, 0, 136, 0, 0, 4, 2, 32, 0, 2, 0, 8, 0, 0, 20, 0, 32, 0, 4, 0, 16, 0, 0, 16, 0, 80, 0, 4, 0, 8, 0, 0, 16, 0, 32, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ord('1'): [32, 0, 0, 0, 8, 128, 20, 0, 2, 32, 0, 0, 0, 136, 0, 20, 0, 2, 0, 0, 0, 5, 8, 0, 20, 0, 10, 0, 0, 0, 10, 10, 128, 40, 0, 10, 2, 0, 0, 2, 10, 136, 20, 0, 10, 2, 64, 0, 2, 1, 17, 40, 0, 9, 0, 64, 0, 0, 32, 1, 84, 0, 8, 128, 64, 0, 0, 32, 1, 80, 0, 0, 128, 0, 128, 0, 32, 0, 160, 0, 0, 0, 0, 128, 0, 0, 0, 32, 0, 0, 0, 0, 128, 0, 0, 0, 36, 0, 0, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 40, 0, 0, 0, 0, 0, 0, 0, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ord('2'): [21, 85, 0, 0, 0, 0, 0, 0, 0, 21, 85, 0, 0, 0, 0, 0, 0, 0, 21, 64, 0, 128, 0, 0, 0, 32, 0, 42, 160, 0, 160, 0, 0, 128, 160, 0, 20, 64, 0, 128, 0, 2, 162, 160, 0, 16, 0, 0, 0, 0, 129, 85, 64, 0, 16, 0, 0, 0, 0, 0, 85, 0, 0, 0, 0, 0, 0, 0, 0, 42, 0, 0, 0, 0, 0, 0, 0, 0, 40, 0, 0, 0, 0, 64, 0, 0, 0, 16, 0, 0, 0, 0, 64, 0, 0, 0, 34, 0, 0, 0, 0, 128, 0, 0, 0, 16, 0, 0, 0, 0, 64, 0, 0, 0, 34, 0, 0, 0, 0, 128, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ord('3'): [40, 0, 0, 21, 64, 0, 0, 4, 1, 84, 0, 1, 21, 64, 40, 0, 4, 1, 80, 0, 5, 0, 128, 168, 0, 4, 5, 64, 32, 10, 2, 2, 168, 128, 0, 10, 0, 32, 10, 1, 5, 8, 0, 0, 4, 8, 32, 10, 0, 2, 0, 0, 0, 0, 8, 0, 10, 0, 4, 64, 0, 0, 0, 16, 0, 21, 0, 4, 0, 0, 0, 0, 16, 0, 5, 0, 20, 0, 0, 4, 4, 16, 0, 2, 41, 20, 0, 0, 16, 36, 0, 0, 0, 80, 16, 0, 0, 0, 168, 0, 0, 0, 0, 0, 0, 0, 0, 80, 0, 0, 0, 0, 0, 0, 0, 0, 168, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ord('4'): [64, 0, 0, 0, 1, 0, 0, 0, 1, 64, 0, 0, 0, 1, 64, 0, 0, 5, 65, 0, 0, 0, 1, 64, 0, 0, 21, 65, 0, 0, 0, 1, 68, 0, 0, 42, 65, 0, 0, 0, 1, 4, 0, 0, 80, 65, 0, 0, 0, 1, 4, 0, 0, 0, 128, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 32, 0, 0, 0, 8, 0, 0, 0, 0, 32, 0, 0, 0, 1, 0, 0, 0, 0, 128, 0, 0, 0, 2, 0, 0, 0, 0, 64, 0, 0, 0, 1, 0, 0, 0, 0, 64, 0, 0, 0, 1, 0, 0, 0, 0, 64, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ord('5'): [20, 0, 0, 0, 0, 0, 0, 0, 0, 84, 0, 0, 0, 0, 0, 0, 0, 1, 80, 0, 0, 0, 0, 0, 0, 0, 5, 73, 0, 0, 0, 2, 37, 85, 0, 10, 136, 0, 8, 0, 2, 42, 170, 64, 10, 136, 0, 2, 128, 2, 0, 0, 128, 1, 0, 0, 2, 0, 5, 0, 0, 16, 0, 10, 0, 4, 0, 4, 0, 0, 16, 0, 20, 0, 4, 0, 0, 0, 0, 16, 0, 0, 0, 4, 0, 0, 0, 0, 16, 0, 0, 0, 2, 0, 0, 0, 0, 8, 0, 0, 0, 4, 0, 0, 0, 0, 40, 0, 0, 0, 0, 0, 0, 0, 0, 80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ord('6'): [42, 170, 0, 0, 0, 0, 0, 0, 0, 42, 170, 0, 0, 0, 0, 0, 0, 0, 42, 128, 0, 0, 0, 0, 0, 0, 0, 85, 64, 0, 0, 0, 1, 0, 0, 0, 40, 128, 0, 0, 0, 5, 64, 0, 0, 32, 0, 0, 0, 1, 2, 144, 0, 0, 32, 2, 0, 0, 0, 0, 68, 0, 0, 0, 1, 0, 0, 0, 0, 4, 0, 0, 0, 2, 16, 0, 0, 0, 8, 0, 0, 0, 0, 80, 0, 0, 0, 0, 0, 0, 0, 0, 168, 0, 0, 0, 0, 0, 0, 0, 0, 80, 0, 0, 0, 0, 0, 0, 0, 0, 168, 0, 0, 0, 0, 0, 0, 0, 0, 80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+}
+
+def expand_icon(icon_data):
+    # Calculate the width and height of the icon
+    icon_width = 36
+    icon_height = 36
+
+    # Convert the compressed icon data to binary representation
+    binary_representation = ''.join([format(byte, '08b') for byte in icon_data])
+
+    # Ensure that the binary representation is padded to the correct length
+    binary_representation = binary_representation.ljust(icon_width * icon_height, '0')
+
+    # Create a 2D binary matrix
+    binary_matrix = [[int(binary_representation[row * icon_width + col]) for col in range(icon_width)] for row in range(icon_height)]
+
+    return binary_matrix
+
+# Print the expanded binary representation of each icon
+for key, value in icons.items():
+    binary_matrix = expand_icon(value)
+    print(f"Icon '{chr(key)}':")
+    for row in binary_matrix:
+        print(' '.join(map(str, row)))
+    print()
+
+# Print the expanded binary representation of each icon
+for key, value in icons.items():
+    binary_matrix = expand_icon(value)
+    print(f"Icon '{chr(key)}':")
+    for row in binary_matrix:
+        print(' '.join(map(str, row)))
+    print()
+
 
 def get_char_representation(char):
+    return font.get (ord (char),
+                     [0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000])
+
+def get_icon_representation(char):
     return font.get (ord (char),
                      [0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000, 0b00000000])
 
@@ -171,6 +203,22 @@ def draw_text(text, x, y, scale=1):
         x += char_width * scale  # Move to the next character, accounting for scaling
 
 
+def draw_icon(icon, x, y, scale=1):
+    icon_representation = get_icon_representation(icon)
+    icon_height = 36
+    icon_width = 36  # Assuming a fixed width of 36 for each icon
+
+    for row in reversed (range (icon_height)):
+        for col in reversed (range (icon_width)):
+            pixel = icon_representation[row] & (1 << col)
+            if pixel != 0:
+                # Scale the pixel coordinates by 'scale' to make the character bigger
+                for i in range(scale):
+                    for j in range(scale):
+                        set_pixel(x + (icon_width - col - 1) * scale + i, y + (icon_height - row - 1) * scale + j)
+
+    x += icon_width * scale  # Move to the next character, accounting for scaling
+
 def prepare_text(text, textScale, width, height, offsetx, offsety):
     text_x = (width - len (text) * 8 * textScale) // 2  # Assuming 8 pixels per character
     text_y = height // 2 - 8  # Assuming 8-pixel font height
@@ -185,25 +233,15 @@ while True:
     # Create a blank monochromatic image of size 800x480 (all pixels initialized to 0)
     pixels = bytearray ([0] * (width * height // 8))  # 1 byte per 8 pixels
 
+    ############################################### RENDER ##################################
     # Left column
-    prepare_text("Drive l.: " + read_data(driveLevelPos)[0:3], 2, width, height, -150, -60)
-    prepare_text("Night m.: " + read_data(nightModePos)[0:3], 2, width, height, -150, -30)
-    prepare_text("Distance: " + read_data(distancePos)[0:3], 2, width, height, -150, 0)
-    prepare_text("Gear    : " + read_data(gearPos)[0:3], 2, width, height, -150, 30)
-    prepare_text("Fuel low: " + read_data(fuelLevelLowPos)[0:3], 2, width, height, -150, 60)
-    prepare_text("Speed   : " + read_data(speedPos)[0:3], 2, width, height, -150, 90)
-    prepare_text("Sat v.  : " + read_data(satellitesInViewPos)[0:3], 2, width, height, -150, 120)
-    prepare_text("Sat u.  : " + read_data(satellitesInUsePos)[0:3], 2, width, height, -150, 150)
+    prepare_text("D:" + read_data(distancePos)[0:3], 8, width, height, -150, -60)
+    prepare_text(read_data(speedPos)[0:3], 8, width, height, -150, 30)
 
     # Right column
-    prepare_text("GPS lat    : " + read_data(gpsLatPos)[0:3], 2, width, height, 150, -60)
-    prepare_text("GPS long   : " + read_data(gpsLongPos)[0:3], 2, width, height, 150, -30)
-    prepare_text("GPS acc    : " + read_data(gpsAccPos)[0:3], 2, width, height, 150, 0)
-    prepare_text("GPS alt    : " + read_data(gpsAltPos)[0:3], 2, width, height, 150, 30)
-    prepare_text("GPS speed  : " + read_data(gpsSpeedPos)[0:3], 2, width, height, 150, 60)
-    prepare_text("GPS bear   : " + read_data(gpsBearingPos)[0:3], 2, width, height, 150, 90)
-    prepare_text("Park brake : " + read_data(parkingBrakePos)[0:3], 2, width, height, 150, 120)
-    prepare_text("GPS time   : " + read_data(gpsTimestampPos)[0:3], 2, width, height, 150, 150)
+    prepare_text("GPS lat    : " + read_data("arrow"), 2, width, height, 150, -60)
+    expand_icon("0")
+    draw_icon("0",150,-60,1)
 
     # BMP header for a monochromatic (1-bit) BMP
     bmp_header = struct.pack ('<2sIHHI', b'BM', len (pixels) + 62, 0, 0, 62)
